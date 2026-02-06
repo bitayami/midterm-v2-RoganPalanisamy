bcd1f56445af6e8a1c7b76103d4a859cae845cf7
Rogan Palanisamy <p.roganpalanisamy1012@gmail.com>
2026-02-06 12:27:05 -0700

    Update treasureMap.js

--- treasureMap.js
+++ treasureMap.js
@@ -9,41 +9,23 @@ It must return an object with this structure:
   coordinates: [ [x, y], [x, y] ... ]
 }
 */
-
 const treasureMap = function (grid) {
-  // Place your solution here
+  let treasureCount = 0;
+  let coordinates = [];
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] === "X") {
+        treasureCount++;
+        coordinates.push([x, y]);
+      }
+    }
+  }
+
+  return {
+    treasureCount,
+    coordinates
+  };
 };
-
-const map1 = [
-  ["O", "O", "X", "O"],
-  ["O", "X", "O", "O"],
-  ["O", "O", "O", "X"],
-];
-
-console.log(treasureMap(map1));
-// Output:
-// {
-//   treasureCount: 3,
-//   coordinates: [ [2, 0], [1, 1], [3, 2] ]
-// }
-
-const map2 = [
-  ["O", "O"],
-  ["O", "O"],
-];
-console.log(treasureMap(map2));
-// { treasureCount: 0, coordinates: [] }
-
-const map3 = [
-  ["X", "O", "O"],
-  ["O", "O", "O"],
-  ["X", "X", "O"],
-  ["O", "O", "X"],
-];
-console.log(treasureMap(map3));
-// {
-//   treasureCount: 4,
-//   coordinates: [ [0, 0], [0, 2], [1, 2], [2, 3] ]
-// }
 
 module.exports = treasureMap;
